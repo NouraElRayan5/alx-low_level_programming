aa20d9358be19485251332e94745b0c1b39da5cc
NouraElRayan5 <Nourakhaled385@gmail.com>
2023-09-12 14:59:19 -0700

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,8 +8,8 @@
 
 void times_table(void)
 {
-  int num, mult, prod;
- for (num = 0; num <= 9; num++)
+int num, mult, prod;
+for (num = 0; num <= 9; num++)
 {_putchar(48);
 for (mult = 1; mult <= 9; mult++)
 {
